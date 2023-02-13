eacb35d79d696d7367edd72f32c136d20b9a3038
ThibaultMariePro <thibault.marie.pro@gmail.com>
2023-02-13 23:22:59 +0100

    ajout d'extension de twig pour gerer le filtre locale=fr | listAll de sortie dans SortieController

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SortieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/sortie', name: 'sortie')]
+
+class SortieController extends AbstractController
+{
+    //TODO : main view sortie (listAll...)
+    #[Route('/', name: '_dashboard')]
+    public function dashboard(
+        SortieRepository $sortieRepository
+    ): Response
+    {
+        $sorties = $sortieRepository->findAll();
+
+        return $this->render(
+            'sortie/dashboard.html.twig',
+            compact("sorties")
+        );
+    }
+
+}
