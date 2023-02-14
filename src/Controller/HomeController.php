e2d37798c39506ad6538d34da3c351f679623750
Lauranne Sarribouette <lauranne.sarribouette@gmail.com>
2023-02-14 15:43:12 +0100

    Gestion profil
    - page de profil
    - ajout barre de nav
    - page de màj profil (formulaire)
    - contrôle des accès via security.yaml
    - gestion des messages flash

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ParticipantFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,5 +15,32 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/profile', name: 'home_profile')]
+    public function seeProfile(): Response
+    {
+        return $this->render('home/profile.html.twig');
+    }
+
+    #[Route('/profile/modify', name: 'home_modify')]
+    public function modifyProfile(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $participant = $this->getUser();
+        dump($participant);
+        $participantForm = $this->createForm(ParticipantFormType::class, $participant);
+        $participantForm->handleRequest($request);
+        if ($participantForm->isSubmitted() && $participantForm->isValid()) {
+            $entityManager->persist($participant);
+            $entityManager->flush();
+            $this->addFlash('success', 'Informations du profil mises à jour !');
+            return $this->redirectToRoute('home_profile');
+        }
+        return $this->render('home/modify.html.twig',
+            compact('participantForm')
+        );
     }
 }
